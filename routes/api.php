b298ce926ea7af332969ea36a504f9c7de87abbf
aziz-boujaada <azizboujaada87@gmail.com>
2026-03-12 11:48:12 +0000

    Add API routes for user authentication and profile management

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/my-profile', [ProfileController::class, 'showProfile']);
+    Route::put('/profile/{id}', [ProfileController::class, 'updateProfile']);
+    Route::put('/profile/{id}/reset-password', [ProfileController::class, 'resetPassword']);
+    Route::delete('/profile/{id}', [ProfileController::class, 'destroyProfile']);
+});
